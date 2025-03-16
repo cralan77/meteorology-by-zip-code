7f5f8aba7bc0a7ebd7c975695ecbe7103fc0a71e
Alan Cristiano <cr.alan9119@gmail.com>
2025-03-16 20:49:32 -0300

    first commit - spring initializr

---
+++ src/main/java/com/crconsult/meteorology/by/zip/code/MeteorologyByZipCodeApplication.java
@@ -0,0 +1,13 @@
+package com.crconsult.meteorology.by.zip.code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeteorologyByZipCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeteorologyByZipCodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crconsult/meteorology/by/zip/code/MeteorologyByZipCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crconsult.meteorology.by.zip.code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeteorologyByZipCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
